ddac1b4e7cb74198dff311b7a30c1e74f726cba1
nkornek <nkornek@gmail.com>
2015-07-27 13:45:15 -0400

    Initial commit

---
+++ Assets/MainInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainInput : MonoBehaviour {
+    public bool canInput;
+    public InputField textInput;
+    public MainParser parser;
+
+	// Use this for initialization
+	void Start () {
+        textInput.Select();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (canInput && !textInput.isActiveAndEnabled)
+        {
+            textInput.gameObject.SetActive(true);
+            textInput.Select();
+        }
+        else if (!canInput && textInput.isActiveAndEnabled)
+        {
+            textInput.gameObject.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SubmitText();
+        }
+        //debug
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            if (canInput)
+            {
+                canInput = false;
+            }
+            else
+            {
+                canInput = true;
+            }
+        }
+	}
+
+    void SubmitText() 
+    {
+        //parse the string
+        string lowerString = textInput.text.ToLower();
+        string[] textString = lowerString.Split(' ', '.', '!', '?', ';');
+        textInput.text = "";
+        parser.Parse(textString);
+        //textInput.Select();
+        canInput = false;
+    }
+}
---
+++ Assets/MainParser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainParser : MonoBehaviour {
+    [Header("Words to look for")]
+    public string[] passingVerbs;
+    public string[] shootingVerbs;
+
+    //count number of keywords
+    int numPass;
+    int numShoot;
+	
+	public void Parse(string[] parseString)
+    {
+        for (int i = 0; i < parseString.Length; i++)
+        {
+            //look for specific verbs in the inputted string
+            for (int p = 0; p < passingVerbs.Length; p++)
+            {
+                if (parseString[i] == passingVerbs[p])
+                {
+                    numPass++;
+                }
+            }
+            for (int s = 0; s < shootingVerbs.Length; s++)
+            {
+                if (parseString[i] == passingVerbs[s])
+                {
+                    numShoot++;
+                }
+            }
+        }
+    }
+}
